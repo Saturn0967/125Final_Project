822c23e196bba737ab95ecef4fcd19c3745a10e5
Tianjie Qiu <tianjie3@illinois.edu>
2019-12-02 14:50:34 -0600

    14点50分

--- app/src/main/java/com/example/a125finalproject/MainActivity.java
+++ app/src/main/java/com/example/a125finalproject/MainActivity.java
@@ -1,5 +1,20 @@
 package com.example.a125finalproject;
 
-public class MainActivity {
-    
+import android.os.Bundle;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public final class MainActivity extends AppCompatActivity{
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
 }
